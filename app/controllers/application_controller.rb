a59f4379c1f935908d3d6ade6d9e7fca6c369d6c
serifko <martin.puchly@gmail.com>
2020-11-09 21:00:09 +0100

    devise and bootstrap 4.5 installed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    helper_method :is_super_user
+
+    def is_super_user
+        return current_user.id==1 if user_signed_in?
+        false
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#index" 
+  devise_for :users, path: '', path_names: { sign_in: 'prihlasit', sign_out: 'odhlasit', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'registracia', sign_up: '' }
+
 end
